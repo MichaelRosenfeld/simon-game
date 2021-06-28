d928f220c7d25d5dfb11e5d89ebdfa72897296b8
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-06-28 17:56:51 +0200

    debugged typos : colour---color

--- game.js
+++ game.js
@@ -1,27 +1,30 @@
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour = buttonColours[nextSequence()];
-
+var randomChosenColor = buttonColours[nextSequence()];
+var level = 0;
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
+  level++;
 
-  $("#" + randomChosenColour).fadeOut(200).fadeIn(200).ready(function() {
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  $("#" + randomChosenColor).fadeOut(200).fadeIn(200).ready(function() {
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
   });
 
   return randomNumber;
 }
 
-gamePattern.push(randomChosenColour);
+gamePattern.push(randomChosenColor);
 
 
 $(".btn").click(function() {
 var userChosenColor = $(this).attr("id");
 userClickedPattern.push(userChosenColor);
 });
+
+checkAnswer(userClickedPattern[level]);
 
 function playSound(name) {
   $("#" + name).fadeOut(200).fadeIn(200).ready(function() {
@@ -42,7 +45,18 @@ function animatePress(currentColor) {
 
 $(document).keypress(function() {
   nextSequence();
+  $("h1").text("Level " + level);
 });
+
+// Checking correct Sequence
+
+function checkAnswer(currentLevel) {
+  if(currentLevel === gamePattern){
+    console.log("success");
+  } else {
+    console.log("wrong");
+  }
+}
 
 
 
