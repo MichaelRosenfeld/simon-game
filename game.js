17f7b1ea05db68f412e3f2ffa457ac88008fded5
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-06-28 11:54:44 +0200

    Added an Eventlistener on clicking the buttons.The id of buttons gets stored in an Array of the user pattern.

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
+var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColour = buttonColours[nextSequence()];
+
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
@@ -13,6 +15,32 @@ $("#" + randomChosenColour).fadeOut(200).fadeIn(200).ready(function() {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 });
+
+$(".btn").click(function() {
+var userChosenColor = $(this).attr("id");
+userClickedPattern.push(userChosenColor);
+});
+
+console.log(userClickedPattern);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
