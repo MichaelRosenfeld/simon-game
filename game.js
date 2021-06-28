7a7ecbfb2b236059d19938ec3a05695e34a4660f
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-06-28 12:16:56 +0200

    Copied nextSequence code block to playSound

--- game.js
+++ game.js
@@ -6,15 +6,17 @@ var randomChosenColour = buttonColours[nextSequence()];
 
 function nextSequence(){
   var randomNumber = Math.floor(Math.random() * 4);
+
+  $("#" + randomChosenColour).fadeOut(200).fadeIn(200).ready(function() {
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+  });
+
   return randomNumber;
 }
 
 gamePattern.push(randomChosenColour);
 
-$("#" + randomChosenColour).fadeOut(200).fadeIn(200).ready(function() {
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
-});
 
 $(".btn").click(function() {
 var userChosenColor = $(this).attr("id");
@@ -22,7 +24,10 @@ userClickedPattern.push(userChosenColor);
 });
 
 function playSound(name) {
-
+  $("#" + userChosenColor).fadeOut(200).fadeIn(200).ready(function() {
+    var audio = new Audio("sounds/" + userChosenColor + ".mp3");
+    audio.play();
+  });
 }
 
 
